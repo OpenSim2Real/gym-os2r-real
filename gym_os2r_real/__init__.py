15571115d3dfd298734a03db5034c6477b828563
Dawson Horvath <horvath.dawson@gmail.com>
2022-02-24 14:26:09 -0800

    add hopping registered env into init

--- gym_os2r_real/__init__.py
+++ gym_os2r_real/__init__.py
@@ -5,7 +5,7 @@ from gym_os2r import tasks
 __all__ = ['runtimes']
 
 from gym.envs.registration import register
-from gym_os2r.rewards import StraightV1
+from gym_os2r.rewards import StraightV1, HoppingV1
 # from gym_os2r.rewards import BalancingV1, StandingV1, StandingV2, StandingV3, WalkingV1
 
 register(
@@ -16,5 +16,17 @@ register(
             'agent_rate': 1000,
             'task_mode': 'simple',
             'reward_class': StraightV1,
+            'reset_positions': ['real']
+            })
+
+
+register(
+    id = 'Real-monopod-hop-v1',
+    entry_point = 'gym_os2r_real.runtimes.realtime_runtime:RealTimeRuntime',
+    max_episode_steps = 100000,
+    kwargs={'task_cls': tasks.monopod.MonopodTask,
+            'agent_rate': 1000,
+            'task_mode': 'fixed_hip',
+            'reward_class': HoppingV1,
             'reset_positions': ['real']
             })
