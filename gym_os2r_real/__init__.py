7f9efa057fc987332a1e40af21ed1f830c0cb7a5
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-08 14:55:19 -0800

    meassured torque model registered env

--- gym_os2r_real/__init__.py
+++ gym_os2r_real/__init__.py
@@ -5,12 +5,7 @@ from gym_os2r import tasks
 __all__ = ['runtimes']
 
 from gym.envs.registration import register
-<<<<<<< HEAD
-from gym_os2r.rewards import StraightV1, HoppingV1, BalancingV2
-=======
-from gym_os2r.rewards import StraightV1, HoppingV1
->>>>>>> 15571115d3dfd298734a03db5034c6477b828563
-# from gym_os2r.rewards import BalancingV1, StandingV1, StandingV2, StandingV3, WalkingV1
+from gym_os2r.rewards import *
 
 register(
     id = 'Real-monopod-simple-v1',
@@ -34,7 +29,6 @@ register(
             'reward_class': HoppingV1,
             'reset_positions': ['real']
             })
-<<<<<<< HEAD
 
 
 register(
@@ -47,5 +41,14 @@ register(
             'reward_class': BalancingV2,
             'reset_positions': ['real']
             })
-=======
->>>>>>> 15571115d3dfd298734a03db5034c6477b828563
+
+register(
+    id = 'Real-monopod-balance-v2',
+    entry_point = 'gym_os2r_real.runtimes.realtime_runtime:RealTimeRuntime',
+    max_episode_steps = 100000,
+    kwargs={'task_cls': tasks.monopod.MonopodTask,
+        'agent_rate': 1000,
+        'task_mode': 'fixed_hip_torque',
+        'reward_class': BalancingV3,
+        'reset_positions': ['real']
+        })
